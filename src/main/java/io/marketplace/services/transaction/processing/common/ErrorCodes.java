17c8690db408a82e69029f2948059726ace24d23
DipenkumarMonani <Dipenkumar.Monani@101digital.io>
2023-08-17 12:00:00 +0530

    fix(APB-6642): fix error codes

--- src/main/java/io/marketplace/services/transaction/processing/common/ErrorCodes.java
+++ src/main/java/io/marketplace/services/transaction/processing/common/ErrorCodes.java
@@ -8,11 +8,11 @@ import lombok.Getter;
 public enum ErrorCodes {
 
     //400
-    MISSING_CONFIGURATION_TYPE("000.01.400.01","Missing Configuration Type."),
-    MISSING_CONFIGURATION_LOGIC_CODE("000.01.400.02","Missing Configuration Logic Code."),
-    INVALID_CONFIGURATION_LOGIC_CODE("000.01.400.03","Invalid Configuration Logic Code."),
-    INVALID_CONFIGURATION_TYPE("000.01.400.04","Invalid Configuration Type."),
-    ALREADY_CONFIGURATION_CREATED("000.01.400.05","Configuration is already created"),
+    MISSING_CONFIGURATION_TYPE("161.01.400.01","Missing Configuration Type."),
+    MISSING_CONFIGURATION_LOGIC_CODE("161.01.400.02","Missing Configuration Logic Code."),
+    INVALID_CONFIGURATION_LOGIC_CODE("161.01.400.03","Invalid Configuration Logic Code."),
+    INVALID_CONFIGURATION_TYPE("161.01.400.04","Invalid Configuration Type."),
+    ALREADY_CONFIGURATION_CREATED("161.01.400.05","Configuration is already created"),
 
     //403
 
@@ -21,7 +21,7 @@ public enum ErrorCodes {
     //409
 
     //500
-    ERROR_WHILE_ADD_CONFIGURATIONS("000.01.500.01","Error While Adding Configuration.");
+    ERROR_WHILE_ADD_CONFIGURATIONS("161.01.500.01","Error While Adding Configuration.");
 
     private final String code;
     private final String message;
