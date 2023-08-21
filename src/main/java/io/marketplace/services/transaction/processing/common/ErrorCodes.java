cfe156ffa45aacc88d108ad5cea41105400064d5
DipenkumarMonani <Dipenkumar.Monani@101digital.io>
2023-08-21 10:21:15 +0530

    feat(APB-6644): View RC API

--- src/main/java/io/marketplace/services/transaction/processing/common/ErrorCodes.java
+++ src/main/java/io/marketplace/services/transaction/processing/common/ErrorCodes.java
@@ -13,6 +13,7 @@ public enum ErrorCodes {
     INVALID_CONFIGURATION_LOGIC_CODE("161.01.400.03", "Invalid Configuration Logic Code."),
     INVALID_CONFIGURATION_TYPE("161.01.400.04", "Invalid Configuration Type."),
     ALREADY_CONFIGURATION_CREATED("161.01.400.05", "Configuration is already created"),
+    MISSING_WALLET_ID("161.01.400.06", "Missing WalletId"),
 
     // 403
 
@@ -22,7 +23,10 @@ public enum ErrorCodes {
 
     // 500
     ERROR_WHILE_ADD_CONFIGURATIONS("161.01.500.01", "Error While Adding Configuration."),
-    ERR_DELETE_DB_ERROR("161.01.500.02", "Error occurred when deleting Configuration record.");
+    ERR_DELETE_DB_ERROR("161.01.500.02", "Error occurred when deleting Configuration record."),
+    ERROR_WHILE_GET_CONFIGURATIONS("161.01.500.03", "Error while getting configurations"),
+    ERROR_GETTING_RESPONSE("161.01.500.04", "Error while getting response"),
+    ERROR_CALL_WALLET_SERVICE("161.01.500.05","Error While Calling Wallet Service");
 
     private final String code;
     private final String message;
