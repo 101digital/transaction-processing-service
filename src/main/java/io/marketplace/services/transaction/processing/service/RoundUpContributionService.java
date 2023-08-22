40f99122a9b975e6a0f93e25c889aacb93fe1fcc
Pamitha Lakbodha <waplak@users.noreply.github.com>
2023-08-23 08:21:46 +1000

    feat(APB-6669): add logs for round up eligible check

--- src/main/java/io/marketplace/services/transaction/processing/service/RoundUpContributionService.java
+++ src/main/java/io/marketplace/services/transaction/processing/service/RoundUpContributionService.java
@@ -43,16 +43,16 @@ public class RoundUpContributionService {
 	private static final String MERCHANT_CATERGORY_CODE = "merchant_category_code";
 	private static final String EMPTY_VALUE = "";
 	private final String CONSUMER_CODE = "ADB";
-    private final String TRANSACTION_TYPE = "OWN_ACCOUNTS_TRANSFER";
+	private final String TRANSACTION_TYPE = "OWN_ACCOUNTS_TRANSFER";
 
 	@Value("#{'${roundUpConfig.eligibleBankTransactionCodes}'.split(',')}")
 	private List<String> eligibleBankTransactionCodes;
 
 	@Value("${roundUpConfig.type:ROUNDUP_CONTRIBUTION}")
 	private String roundUpConfigType;
-	
-    @Value("${transaction-processing-config.contribution-param-name:contributionWalletId}")
-    private String contributionWalletId;
+
+	@Value("${transaction-processing-config.contribution-param-name:contributionWalletId}")
+	private String contributionWalletId;
 
 	@Autowired
 	WalletClient walletClient;
@@ -66,58 +66,61 @@ public class RoundUpContributionService {
 	public void processTransaction(OBTransaction6 transaction) {
 		Optional<ConfigurationEntity> optConfig = isTransactionEligible(transaction);
 		if (!optConfig.isPresent()) {
-			log.info("Transaction not supported to round up configuration {}", gson.toJson(transaction));
+			log.info("Transaction not eligible to round up contribution {}", gson.toJson(transaction));
 			return;
 		}
-		
+
 		log.info("Round up process started {}", gson.toJson(transaction));
 		ConfigurationEntity configurationEntity = optConfig.get();
-		OBActiveOrHistoricCurrencyAndAmount9 amount = Optional.ofNullable(transaction)
-				.map(OBTransaction6::getAmount)
+		OBActiveOrHistoricCurrencyAndAmount9 amount = Optional.ofNullable(transaction).map(OBTransaction6::getAmount)
 				.orElse(new OBActiveOrHistoricCurrencyAndAmount9());
-		
+
 		BigDecimal roundUpAmount = getRoundUpAmount(configurationEntity, amount.getAmount());
-		
-		if(roundUpAmount.compareTo(BigDecimal.ZERO)>0) {
-			WalletFundTransferRequest walletFundTransferRequest =
-	                toWalletFundTransferRequest(configurationEntity, roundUpAmount, amount.getCurrency());
+
+		if (roundUpAmount.compareTo(BigDecimal.ZERO) > 0) {
+			WalletFundTransferRequest walletFundTransferRequest = toWalletFundTransferRequest(configurationEntity,
+					roundUpAmount, amount.getCurrency());
 			log.info("Round up WalletFundTransferRequest: {}", gson.toJson(walletFundTransferRequest));
-			
-			WalletFundTransferResponse walletFundTransferResponse =
-	                walletClient.walletFundTransfer(
-	                        walletFundTransferRequest, transaction.getUserId());
+
+			WalletFundTransferResponse walletFundTransferResponse = walletClient
+					.walletFundTransfer(walletFundTransferRequest, transaction.getUserId());
 			log.info("Round up WalletFundTransferResponse: {}", gson.toJson(walletFundTransferResponse));
 		}
 	}
-	
+
 	private BigDecimal getRoundUpAmount(ConfigurationEntity configurationEntity, String amount) {
 		switch (configurationEntity.getLogicCode()) {
-        case "DEFAULT":
-        	return calculateCeilingDifference(amount);
-        case "TODO":
-        	return new BigDecimal("0.00"); // implement another round up logic
-        default:
-        	return calculateCeilingDifference(amount);
-    	}
+		case "DEFAULT":
+			return calculateCeilingDifference(amount);
+		case "TODO":
+			return new BigDecimal("0.00"); // implement another round up logic
+		default:
+			return calculateCeilingDifference(amount);
+		}
 	}
-	
+
 	private BigDecimal calculateCeilingDifference(String amount) {
 		BigDecimal txnAmount = new BigDecimal(amount);
-    	BigDecimal ceilingValue = txnAmount.setScale(0, RoundingMode.CEILING);
-    	return ceilingValue.subtract(txnAmount);
-    }
+		BigDecimal ceilingValue = txnAmount.setScale(0, RoundingMode.CEILING);
+		return ceilingValue.subtract(txnAmount);
+	}
 
 	private Optional<ConfigurationEntity> isTransactionEligible(OBTransaction6 transaction) {
 		Optional<ConfigurationEntity> optConfig = Optional.empty();
+		log.info("Round up process eligibility check started {}", gson.toJson(transaction));
 		if (OBEntryStatus1Code.COMPLETED != transaction.getStatus()
 				|| OBCreditDebitCode1.DEBIT != transaction.getCreditDebitIndicator()) {
+			log.info("Not Eligible for round up due to Status: {} and CreditDebitIndicator: {} ",
+					transaction.getStatus().getValue(), transaction.getCreditDebitIndicator().getValue());
 			return optConfig;
 		}
 
 		String bankTransactionCode = Optional.ofNullable(transaction).map(OBTransaction6::getBankTransactionCode)
 				.map(OBBankTransactionCodeStructure1::getCode).orElse(EMPTY_VALUE);
 
+		log.info("Eligible bank transaction codes: {}", eligibleBankTransactionCodes);
 		if (!eligibleBankTransactionCodes.contains(bankTransactionCode) || !validateMerchantCategoryCode(transaction)) {
+			log.info("Not Eligible for round up due to not supported bank transaction code: {} or mcc ", bankTransactionCode);
 			return optConfig;
 		}
 
@@ -125,6 +128,7 @@ public class RoundUpContributionService {
 				.map(OBCashAccount61::getIdentification)
 				.orElseThrow(() -> new BadRequestException(ErrorCodes.MISSING_DEBITOR_ACCOUNT.getCode(),
 						ErrorCodes.MISSING_DEBITOR_ACCOUNT.getMessage(), gson.toJson(transaction)));
+		log.info("Get WalletId By Account Number {}", debitorAccountNumber);
 		String walletId = getWalletIdByAccountNumber(debitorAccountNumber);
 		log.info("Debitor account walletId {}", walletId);
 		if (walletId != null) {
@@ -140,7 +144,7 @@ public class RoundUpContributionService {
 								.map(OBMerchantDetails1::getMerchantCategoryCode).orElse(EMPTY_VALUE))
 				.filter(str -> !str.isEmpty()).findFirst().orElse(EMPTY_VALUE);
 		if (StringUtils.isEmpty(mcc) || "0000".equals(mcc)) {
-			log.info("Merchant Category Code validation failed{}", mcc);
+			log.info("Merchant category Code not supported {}", mcc);
 			return false;
 		}
 		return true;
@@ -152,33 +156,27 @@ public class RoundUpContributionService {
 				.map(WalletListResponse::getData).stream().flatMap(Collection::stream).findFirst()
 				.map(Wallet::getWalletId).orElse(null);
 	}
-	
-	private WalletFundTransferRequest toWalletFundTransferRequest(ConfigurationEntity configurationEntity, BigDecimal roundUpAmount, String currency) {
+
+	private WalletFundTransferRequest toWalletFundTransferRequest(ConfigurationEntity configurationEntity,
+			BigDecimal roundUpAmount, String currency) {
 		String debitorWalletId = configurationEntity.getWallet();
 		Optional<ConfigurationParamEntity> configurationParamEntity = configurationEntity.getConfigurationParamList()
-				.stream().filter(param -> contributionWalletId.equals(param.getParamName()))
-				.findFirst();
-		if(!configurationParamEntity.isPresent()) {
+				.stream().filter(param -> contributionWalletId.equals(param.getParamName())).findFirst();
+		if (!configurationParamEntity.isPresent()) {
 			throw new BadRequestException(ErrorCodes.ERR_SAVINGS_POT_NOT_FOUND_ERROR.getCode(),
-					ErrorCodes.ERR_SAVINGS_POT_NOT_FOUND_ERROR.getMessage(),"");
+					ErrorCodes.ERR_SAVINGS_POT_NOT_FOUND_ERROR.getMessage(), "");
 		}
-		return WalletFundTransferRequest.builder()
-                .consumerCode(CONSUMER_CODE)
-                .transactionType(TRANSACTION_TYPE)
-                .transaction(Transaction.builder()
-                		.amount(roundUpAmount)
-                		.currencyCode(currency)
-                		.creditor(Account.builder()
-                				.wallet(WalletFundTransferRequest.Wallet.builder()
-                						.walletId(configurationParamEntity.get().getValue()).build()
-                						).build()
-                				)
-                		.debtor(Account.builder()
-                				.wallet(WalletFundTransferRequest.Wallet.builder()
-                						.walletId(debitorWalletId).build()
-                						).build()
-                				).build()
-                		).build();
+		return WalletFundTransferRequest.builder().consumerCode(CONSUMER_CODE).transactionType(TRANSACTION_TYPE)
+				.transaction(Transaction.builder().amount(roundUpAmount).currencyCode(currency)
+						.creditor(Account.builder()
+								.wallet(WalletFundTransferRequest.Wallet.builder()
+										.walletId(configurationParamEntity.get().getValue()).build())
+								.build())
+						.debtor(Account.builder()
+								.wallet(WalletFundTransferRequest.Wallet.builder().walletId(debitorWalletId).build())
+								.build())
+						.build())
+				.build();
 	}
 
 }
