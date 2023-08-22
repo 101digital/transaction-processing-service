cb665330db58f5f4b1a5719b353d7189223ad8fb
DipenkumarMonani <Dipenkumar.Monani@101digital.io>
2023-08-22 15:14:19 +0530

    feat(APB-6643): repo sync up

--- src/main/java/io/marketplace/services/transaction/processing/api/ConfigurationsApiDelegateImpl.java
+++ src/main/java/io/marketplace/services/transaction/processing/api/ConfigurationsApiDelegateImpl.java
@@ -1,9 +1,12 @@
 package io.marketplace.services.transaction.processing.api;
 
 import io.marketplace.services.transaction.processing.model.Configurations;
+import io.marketplace.services.transaction.processing.model.ConfigurationsListResponse;
 import io.marketplace.services.transaction.processing.model.ConfigurationsResponse;
 import io.marketplace.services.transaction.processing.service.ConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,28 @@ public class ConfigurationsApiDelegateImpl implements ConfigurationsApiDelegate 
     public ResponseEntity<ConfigurationsResponse> addConfigurations(Configurations configurations) {
 
         return ResponseEntity.ok().body(configurationService.addConfigurations(configurations));
+    }
+
+    @Override
+    public ResponseEntity<String> deleteConfigurationById(String configurationId) {
+        configurationService.deleteConfigurationById(configurationId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    public ResponseEntity<ConfigurationsListResponse> getConfigurations(
+            String walletId, String type, Integer pageNumber, Integer pageSize, String listOrders) {
+        return ResponseEntity.ok()
+                .body(
+                        configurationService.getConfigurations(
+                                walletId, type, pageNumber, pageSize, listOrders));
+    }
+
+    @Override
+    public ResponseEntity<ConfigurationsResponse> updateConfigurationById(String configurationId,
+        Configurations configurations) {
+
+        return ResponseEntity.ok().body(configurationService.updateConfigurationById(configurationId,configurations));
+
     }
 }
