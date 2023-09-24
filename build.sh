d713146100fcf9513e8a165d098625e413bd0df3
Alex Lee <alex@AMB-24.local>
2023-09-24 08:19:58 +0800

    APE-2234 fix chart mapping

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ baseDir=$(pwd)
 echo "building app"
 cd "${baseDir}"
 mvn clean package -DskipTests
-time docker build -t containers.github.acsdigital.dev/adb/transaction-processing-service:1.0.0-SNAPSHOT .
-docker push containers.github.acsdigital.dev/adb/transaction-processing-service:1.0.0-SNAPSHOT
+time docker build -t containers.github.acsdigital.dev/adb/transaction-processing-service:1.0.0-LOSNAPSHOT .
+docker push containers.github.acsdigital.dev/adb/transaction-processing-service:1.0.0-LOSNAPSHOT
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd charts/transaction-processing-service-chart
+cd charts/transaction-processing-service
 helm uninstall transaction-processing-service -n adb
-helm install transaction-processing-service . -f values-$1.yaml -n adb --create-namespace
+helm install transaction-processing-service . -f values-$1.yaml --set image.tag=1.0.0-LOSNAPSHOT -n adb --create-namespace
