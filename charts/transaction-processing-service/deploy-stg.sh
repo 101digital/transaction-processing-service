4963a6a66edf86a1b731c2adfe54f3c992482578
Alex Lee <alex@AMB-24.local>
2023-11-06 06:34:36 +0800

    update deploy.sh

--- charts/transaction-processing-service/deploy-stg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd charts/account-statement-service
-helm uninstall account-statement-service -n $2
-helm install account-statement-service . -f values-$1.yaml -n $2 --set image.tag=1.0.0-LOSNAPSHOT --create-namespace --set image.tag=1.0.0-LOSNAPSHOT
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd charts/transaction-processing-service
-helm uninstall transaction-processing-service -n adb-dev
-helm install transaction-processing-service . -f values-$1.yaml -n adb-dev --create-namespace --set image.tag=1.0.0-LOSNAPSHOT
+helm uninstall transaction-processing-service -n $2
+helm install transaction-processing-service . -f values-$1.yaml -n $2 --create-namespace --set image.tag=1.0.0-LOSNAPSHOT
